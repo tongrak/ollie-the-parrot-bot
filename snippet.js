827eb683094dc93e5dbbff2b41480a7fb7a75a7a
Oliver Tongrak <oliver.tongrak@gmail.com>
2024-07-22 18:31:22 +0700

    Fix undefined variable problem

--- snippet.js
+++ snippet.js
@@ -1,3 +1,5 @@
+import {InteractionResponseType} from 'discord-interactions'; 
+
 export const getSimpleMessageBody = (content) => ({
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
